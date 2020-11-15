ccf37be26c94a8fb24a08366f600a96a985e809e
cornwallluke <luke@cornwall-jones.com>
2020-11-15 19:30:23 +0000

    removed dep

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddModalComponent } from './add-modal/add-modal.component';
@@ -14,8 +13,7 @@ import { HolderComponent } from './holder/holder.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
